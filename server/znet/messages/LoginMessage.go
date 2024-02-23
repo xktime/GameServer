fdc52a0d07e58fd70ac2c15efa050cbcd7f435bb
xktime <luoliwen0913@foxmail.com>
2024-02-23 17:14:50 +0800

    【Login未注册时处理】

--- server/znet/messages/LoginMessage.go
+++ server/znet/messages/LoginMessage.go
@@ -18,9 +18,17 @@ func (login *ReqLoginMessage) DoAction() error {
 	fmt.Println("receive login message")
 	user, err := db.FindByAccount(login.Account)
 	if err != nil {
-		db.RegisterUser(login.ServerId, login.Account)
-		return err
+		if err.Error() != "mongo: no documents in result" {
+			return err
+		}
+		// 如果未注册，先注册再查找
+		err = db.RegisterUser(login.ServerId, login.Account)
+		if err != nil {
+			return err
+		}
+		return login.DoAction()
 	}
+
 	fmt.Println(user)
 	return nil
 }
